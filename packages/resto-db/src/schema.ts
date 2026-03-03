70b0c8a7ff80f778a4c6f3e055a27f8bc1140594
arfath-linklet <arfath@linklet.io>
2026-03-03 14:38:09 +0530

    adds restaurant_availability table schema

--- packages/resto-db/src/schema.ts
+++ packages/resto-db/src/schema.ts
@@ -1,3 +1,30 @@
-import { pgSchema } from "drizzle-orm/pg-core";
+import type { InferSelectModel } from "drizzle-orm";
+import { boolean, pgSchema, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const appPgSchema = pgSchema("app");
+
+export const restaurantAvailabilities = appPgSchema.table(
+	"restaurant_availabilities",
+	{
+		// Identity
+		id: uuid("id").primaryKey().defaultRandom(),
+
+		res_id: text("res_id").notNull().unique(),
+		res_status_text: text("res_status_text"),
+		name: text("name").notNull(),
+		isOpenNow: boolean("is_open_now").notNull(),
+		isPermClosed: boolean("is_perm_closed").notNull(),
+		isTempClosed: boolean("is_temp_closed").notNull(),
+
+		// Timestamps
+		createdAt: timestamp("created_at").defaultNow().notNull(),
+		updatedAt: timestamp("updated_at")
+			.defaultNow()
+			.$onUpdate(() => /* @__PURE__ */ new Date())
+			.notNull(),
+	},
+);
+
+export type RestaurantAvailability = InferSelectModel<
+	typeof restaurantAvailabilities
+>;
